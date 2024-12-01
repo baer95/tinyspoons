9d6c9ba8e707b4bb99fcce8a092c4c345a87820d
Bernhard Frick <bernhard.frick@adevinta.com>
2024-12-01 14:43:08 +0100

    remove about page

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+<!--      <RouterLink to="/about">About</RouterLink>-->
       <RouterLink to="/random-spoon">Random Spoon</RouterLink>
     </nav>
   </header>
